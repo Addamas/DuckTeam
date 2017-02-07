d42a842c92f89312f6929ac5d30f9f33ea50123b
Addamas <kingosman6@gmail.com>
2017-02-07 13:30:00 +0100

    added playercontroller

---
+++ Anomaly/Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+    float rotationY;
+    public float mouseSensitivity;
+    public float minimumY = -90;
+    public float maximumY = 90;
+    void Start () {
+	
+	}
+	
+	void Update () {
+
+        rotationY += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+}
