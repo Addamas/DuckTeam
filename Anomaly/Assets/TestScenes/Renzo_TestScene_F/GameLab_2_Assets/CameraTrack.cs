b9111a65246aac4b7f5a00a724cfeba83b9d8b45
Renzo Fledderus <renzopaso@live.nl>
2017-03-01 11:28:05 +0100

    added crafting table assets en start camera script start

---
+++ Anomaly/Assets/TestScenes/Renzo_TestScene_F/GameLab_2_Assets/CameraTrack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour {
+
+    public List<Transform> anchors = new List<Transform>();
+    public int count = 1;
+
+    public float speed = 0.1F;
+
+    void Start () {
+        transform.LookAt(anchors[0].transform);
+    }
+
+    void Update () {
+        //transform.rotation = Quaternion.Lerp(this.transform.rotation, anchors[0].rotation, Time.time * speed);
+
+        transform.rotation = Quaternion.Lerp(anchors[count-1].rotation, anchors[count].rotation, Time.time * speed);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            count++;
+        }
+	}
+}
+
---
+++ Anomaly/Assets/TestScenes/Renzo_TestScene_F/GameLab_2_Assets/_CraftingTable/Scripts/MouseFollower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MouseFollower : MonoBehaviour {
+
+    public Transform mouseImage;
+    public Vector3 imageOffset;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Image>().enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Input.mousePosition + imageOffset;
+	}
+}
