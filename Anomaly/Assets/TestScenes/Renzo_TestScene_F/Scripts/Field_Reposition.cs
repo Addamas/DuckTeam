fe0154fabb1b24c54c3bb4adbe7ea7b2f3850c3e
Renzo Fledderus <renzopaso@live.nl>
2017-03-17 13:03:44 +0100

    no message

--- Anomaly/Assets/TestScenes/Renzo_TestScene_F/Scripts/Field_Reposition.cs
+++ Anomaly/Assets/TestScenes/Renzo_TestScene_F/Scripts/Field_Reposition.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// ontrigenter, get player.enterPosition, if player.pos.distance > xAmount, player.pos = enterposition.
+// ontriggenter, get player.enterPosition, if player.pos.distance > xAmount, player.pos = enterposition.
 //( increase fog based on distance to field center, moet ook opletten dat de xAmount wordt berekend tot de diepte van de field, en niet wordt getriggerd als je toch terug loopt de map in.
 //      Keep player.lookrotation in mind
 
@@ -12,6 +12,7 @@ public class Field_Reposition : MonoBehaviour {
 
     Vector3 enterPos;
     public Transform playerB;
+    private Quaternion playerRot;
     public Transform playerTracker;
     public Transform trckr;
 
@@ -28,7 +29,8 @@ public class Field_Reposition : MonoBehaviour {
         if (onEnter.transform.tag == "Player")
         {
             enterPos = playerB.position;
-            trckr = Instantiate(playerTracker, playerB.position, Quaternion.identity, this.transform);
+            //playerRot = playerB.rotation;
+            trckr = Instantiate(playerTracker, playerB.position, playerB.rotation, this.transform);
             Debug.Log("Player entered field");
         }
     }
@@ -37,12 +39,12 @@ public class Field_Reposition : MonoBehaviour {
     {
         if (onStay.transform.tag == "Player")
         {
-            trckr.transform.position = new Vector3(enterPos.x, enterPos.y, playerB.position.z);
+            trckr.position = new Vector3(enterPos.x, enterPos.y, playerB.position.z);
             float dist = Vector3.Distance(playerB.position, trckr.position);
             if (dist >= 10)
             {
-                playerB.transform.position = trckr.transform.position;
-                
+                playerB.position = new Vector3 (enterPos.x, enterPos.y, enterPos.z);
+                playerB.rotation = Quaternion.Inverse(trckr.rotation);
             }
         }
     }
