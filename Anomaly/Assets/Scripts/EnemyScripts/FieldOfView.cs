fb840c474a658e36fcf85b325013029a1401bcd3
Tomvhe <tvhe@outlook.com>
2017-03-22 11:31:41 +0100

    Added my scripts (enemy movement)

---
+++ Anomaly/Assets/Scripts/EnemyScripts/FieldOfView.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius = 11f;
+    public float noticeRadius = 3f;
+    [Range(0,360)]
+    public float viewAngle = 140f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    } 
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisableTargets();
+
+        }
+    }
+
+    void FindVisableTargets ()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position,viewRadius,targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            //print(viewAngle / 2);
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle/*/ viewAngle / 2 /*/)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                    gameObject.GetComponent<MainEnemy>().enemyState = EnemyBase.EnemyState.Chasing;
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+            angleInDegrees += transform.eulerAngles.y;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
