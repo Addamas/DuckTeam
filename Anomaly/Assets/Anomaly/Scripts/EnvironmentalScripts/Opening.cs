b246e05b32f8e84c47ef491ffec8128f47e0679d
DESKTOP-DAPG37V\kingo <kingosman6@gmail.com>
2017-05-11 08:50:53 +0200

    tweaks

---
+++ Anomaly/Assets/Anomaly/Scripts/EnvironmentalScripts/Opening.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Opening : MonoBehaviour {
+
+    public GameObject sprite;
+    float alpha;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GameObject.Find("BlackBG");
+        FadeFromBlack();
+	}
+	
+	// Update is called once per frame
+	void FadeFromBlack () {
+        sprite.GetComponent<Image>().GetComponent<Color>().a = alpha;
+        alpha = Mathf.Lerp(1, 0, 1 * Time.deltaTime);
+
+    }
+}
