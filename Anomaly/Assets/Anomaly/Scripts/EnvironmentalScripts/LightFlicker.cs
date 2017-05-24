e1292010d6f09081b941f74bb8dfdbfe23c27c88
Tomvhe <tvhe@outlook.com>
2017-05-24 10:57:56 +0200

    Added light flickering script

---
+++ Anomaly/Assets/Anomaly/Scripts/EnvironmentalScripts/LightFlicker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public bool lightWillFlicker;
+    private bool lightOff;
+    private float originalIntensity;
+    [Header("chance will be 1 in x amount to have a chance of flickering")]
+    [Range(1,100)]
+    public int chanceOfFlicker = 3;
+
+    private void Start()
+    {
+        originalIntensity = GetComponent<Light>().intensity;
+        StartCoroutine(Flickering());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    IEnumerator Flickering ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.09f);
+
+            if (lightWillFlicker)
+            {
+                int i = Random.Range(1, chanceOfFlicker);
+                float f = Random.Range(0,(originalIntensity/3));
+                if (i == 1)
+                {
+                    lightOff = !lightOff;
+                    if (lightOff)
+                    {
+                        GetComponent<Light>().intensity = f;
+                    }
+                    else
+                    {
+                        GetComponent<Light>().intensity = originalIntensity;
+                    }
+                }
+            }
+            else
+            {
+                lightOff = false;
+                GetComponent<Light>().intensity = originalIntensity;
+            }
+        }
+    }
+}
