d7d9dc3c202704d46c9ecefd2962e3253b97058a
Tomvhe <tvhe@outlook.com>
2017-05-30 13:48:26 +0200

    Added scripted event function

--- Anomaly/Assets/Anomaly/Scripts/EnemyScripts/MainEnemy.cs
+++ Anomaly/Assets/Anomaly/Scripts/EnemyScripts/MainEnemy.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainEnemy : EnemyBase{
+public class MainEnemy : EnemyBase
+{
 
-    
     public void Start()
     {
         player = GameObject.FindWithTag("Player");
+
+        //  ScriptedEvent(start.position, end.position, false, true);               start position, end position, can see the player, teleport back to the original place    //
     }
 
     public void Update()
     {
         Movement();
-    } 
-
-
+    }
+    
 }
