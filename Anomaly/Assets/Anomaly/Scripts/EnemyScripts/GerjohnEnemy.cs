75f0aa290fe7a63fd4027dfdcf765eb39b7bfb03
Gerbor <gerjohnvandenbosch@hotmail.com>
2017-06-22 12:54:12 +0200

    More updates

---
+++ Anomaly/Assets/Anomaly/Scripts/EnemyScripts/GerjohnEnemy.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerjohnEnemy : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent agent;
+    public Vector3[] waypoints;
+    public int target;
+    public GameObject player;
+    public bool targetingPlayer;
+    public bool seePlayer;
+    public bool decaying;
+    public int targetRange;
+    public int chasingTime;
+    public float chaseSpeed;
+    public float chaseAcceleration;
+    public AudioSource aClip;
+    public bool freezePos;
+
+    public GameObject bip;
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        SetTarget();
+        player = GameObject.FindGameObjectWithTag("Player");
+        bip.transform.localEulerAngles = new Vector3(-90, 0, 100);
+    }
+
+    void Update()
+    {
+        if (!targetingPlayer && !freezePos)
+        {
+            float dist = Vector3.Distance(transform.position, waypoints[target]);
+            if (transform.position.ToString() == waypoints[target].ToString() || dist < 0.5f)
+            {
+               // aClip.Play();
+                if (target == waypoints.Length - 1)
+                {
+                    target = 0;
+                    SetTarget();
+                    return;
+                }
+                else
+                {
+                    target++;
+                    SetTarget();
+                }
+            }
+        }
+        else
+        {
+            TargetPlayer();
+        }
+    }
+
+    public void FoundPlayer()
+    {
+        print("DETECTED PLAYER");
+        if (!targetingPlayer)
+        {
+            targetingPlayer = true;
+            seePlayer = true;
+        }
+    }
+
+    public void CantFindPlayer()
+    {
+        seePlayer = false;
+        if (targetingPlayer)
+        {
+            if (!decaying)
+            {
+                print("Started decaying");
+                decaying = true;
+                StartCoroutine(Decay());
+            }
+        }
+    }
+
+    IEnumerator Decay()
+    {
+        yield return new WaitForSeconds(chasingTime);
+        if (!seePlayer)
+        {
+            StopChasing();
+        }
+        decaying = false;
+    }
+
+    public void StopChasing()
+    {
+        print("Stoped Chasing player");
+        targetingPlayer = false;
+        SetTarget();
+        agent.speed = 3.5f;
+        agent.acceleration = 8;
+    }
+
+    public void TargetPlayer()
+    {
+        if (!freezePos)
+        {
+            agent.speed = chaseSpeed;
+            agent.acceleration = chaseAcceleration;
+            agent.SetDestination(player.transform.position);
+            if (!seePlayer)
+            {
+                StartCoroutine(Decay());
+            }
+        }
+    }
+
+    public void SetTarget()
+    {
+        agent.SetDestination(waypoints[target]);
+    }
+    public void OnTriggerEnter(Collider c)
+    {
+        if (c.transform.tag == "Player")
+        {
+            StopChasing();
+            print("Player Died!");
+        }
+    }
+}
---
+++ Anomaly/Assets/Anomaly/Scripts/EnvironmentalScripts/Closet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Closet : MonoBehaviour {
+
+    public GameObject phone;
+    public GameObject enemyPos;
+    Animator anim;
+    bool isOpen;
+    public float spawnChance;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Use()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            anim.SetTrigger("Close");
+            return;
+        }
+        else
+        {
+            isOpen = true;
+            float randomInt = Random.Range(0, 100);
+            print(randomInt.ToString());
+            if(randomInt <= spawnChance)
+            {
+                print("Put enemy in closet");
+                //Put enemy in closet
+                enemyPos.SetActive(true);
+                phone.GetComponent<Phone>().InsanityBoost(20);
+                StartCoroutine(EnemyDisapear());
+            }
+            anim.SetTrigger("Open");
+        }
+    }
+    IEnumerator EnemyDisapear()
+    {
+        yield return new WaitForSeconds(1.5f);
+        //Do particle shit
+        enemyPos.SetActive(false);
+    }
+}
